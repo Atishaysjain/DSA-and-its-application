7b33365f2ec9feec6cb63c31ad6112ded2b53b60
Atishay <atishaysjain@gmail.com>
2021-09-02 23:36:02 +0530

    reversing stack using recursion

--- stacks/reverse.cpp
+++ stacks/reverse.cpp
@@ -20,8 +20,89 @@ void reverse_sentence(string s){
     return;
 }
 
+// void insert_bottom(stack<int> *st, int t){
+
+//     if(*st.empty()){
+//         *st.push(t);
+//         return;
+//     }
+//     int top_element = *st.top();
+//     *st.pop();
+//     insert_bottom(st, t);
+//     *st.push(top_element);
+
+//     return;
+// }
+
+// void reverse_stack_recursion(stack<int> *st){
+
+//     // 5 4 3 2 1 -> 4 3 2 1 -> 3 2 1 -> 2 1 -> 1
+
+//     if(*st.empty()){
+//         return;
+//     }
+
+//     int t = *st.top();
+//     *st.pop();
+//     reverse_stack_recursion(st);
+//     insert_bottom(st, t);
+//     return;
+    
+// }
+
+void insert_bottom(stack<int> &st, int t){
+
+    if(st.empty()){
+        st.push(t);
+        return;
+    }
+    int top_element = st.top();
+    st.pop();
+    insert_bottom(st, t);
+    st.push(top_element);
+
+    return;
+}
+
+void reverse_stack_recursion(stack<int> &st){
+
+    // 5 4 3 2 1 -> 4 3 2 1 -> 3 2 1 -> 2 1 -> 1
+
+    if(st.empty()){
+        return;
+    }
+
+    int t = st.top();
+    st.pop();
+    reverse_stack_recursion(st);
+    insert_bottom(st, t);
+    return;
+    
+}
+
+void display(stack<int> st){
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    cout<<endl;
+    return;
+}
+
 int main(){
+
     stack<int> st;
-    string s = "Hey, How are you brother?";
-    reverse_sentence(s);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    cout<<"Before reversing :- "<<endl;
+    display(st);
+    reverse_stack_recursion(st);
+    cout<<"after reversing :- "<<endl;
+    display(st);
+
+    return 0;
 }
